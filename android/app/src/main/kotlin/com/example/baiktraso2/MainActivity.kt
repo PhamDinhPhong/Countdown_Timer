083808abd9872dd9b30f19f18ce17cde2ea045d0
PhamDinhPhong <120767098+PhamDinhPhong@users.noreply.github.com>
2024-03-14 14:38:29 +0700

    first

---
+++ android/app/src/main/kotlin/com/example/baiktraso2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.baiktraso2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_countdown_timer/flutter_countdown_timer.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Countdown Timer',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CountdownScreen(),
+    );
+  }
+}
+
+class CountdownScreen extends StatefulWidget {
+  @override
+  _CountdownScreenState createState() => _CountdownScreenState();
+}
+
+class _CountdownScreenState extends State<CountdownScreen> {
+  int _durationInSeconds = 0;
+  int _endTime = DateTime.now().millisecondsSinceEpoch;
+
+  void _startCountdown() {
+    setState(() {
+      _endTime = DateTime.now().millisecondsSinceEpoch + _durationInSeconds * 60 * 1000;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Countdown Timer'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                keyboardType: TextInputType.number,
+                onChanged: (value) {
+                  setState(() {
+                    _durationInSeconds = int.tryParse(value) ?? 0;
+                  });
+                },
+                decoration: InputDecoration(
+                  labelText: 'Nhập số phút',
+                  border: OutlineInputBorder(),
+                  contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _startCountdown,
+                child: Text('Bắt đầu đếm ngược'),
+              ),
+              SizedBox(height: 20),
+              _durationInSeconds > 0
+                  ? CountdownTimer(
+                endTime: _endTime,
+                textStyle: TextStyle(fontSize: 48),
+              )
+                  : Container(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
